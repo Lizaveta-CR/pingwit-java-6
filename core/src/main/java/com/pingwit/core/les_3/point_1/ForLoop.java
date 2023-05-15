dbc7ef495592ddf9ac38a33994fb1da79fcca7ff
Lizaveta Tsvirko <lizaveta_tsvirko@epam.com>
2023-05-15 20:28:14 +0300

    les_3

---
+++ core/src/main/java/com/pingwit/core/les_3/point_1/ForLoop.java
@@ -0,0 +1,25 @@
+package com.pingwit.core.les_3.point_1;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        /*
+        for (initialization expression; condition; update-expression) {
+            //body
+        }
+         */
+        for (int i = 0; i <= 99; i++) {
+            System.out.println("i = " + i);
+        }
+
+        for (int i = 0; i <= 99; i++) {
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("After for loop");
+
+        for (int i = -3; i <= 3; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ core/src/main/java/com/pingwit/core/les_3/point_1/LogicalOperators.java
@@ -0,0 +1,18 @@
+package com.pingwit.core.les_3.point_1;
+
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+        //& - AND, && - AND, | - OR, || - OR - хотя бы 1 true, ! - NOT
+        boolean a = true;
+        boolean b = false;
+        boolean c = !(a && b);
+        System.out.println(c);
+
+        System.out.println(true || false);
+        System.out.println(!(2 < 5));
+        System.out.println(false & (2 > 5));
+        System.out.println((2 < 5) || false);
+        System.out.println((2 > 5) || true);
+    }
+}
