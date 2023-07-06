37373139de0ff585749a5523824f2ef189d8a091
Lizaveta Tsvirko <lizaveta_tsvirko@epam.com>
2023-07-06 20:31:23 +0300

    les_17

---
+++ core/src/main/java/com/pingwit/core/les_17/classwork/point_1/OIS_Example.java
@@ -0,0 +1,21 @@
+package com.pingwit.core.les_17.classwork.point_1;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class OIS_Example {
+
+    private static final String FILE_PATH = "core/src/main/java/com/pingwit/core/les_17/classwork/point_1/object";
+
+    public static void main(String[] args) {
+        try (ObjectInputStream oos = new ObjectInputStream(new FileInputStream(FILE_PATH))) {
+            Person person = (Person) oos.readObject();
+            System.out.println(person);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
