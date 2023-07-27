409658b4da223881c33936c7e905e412c09a8f62
Lizaveta Tsvirko <lizaveta_tsvirko@epam.com>
2023-07-27 21:01:47 +0300

    les_22

---
+++ core/src/main/java/com/pingwit/core/les_22/classwork/point_1/SimpleFuncInterface.java
@@ -0,0 +1,30 @@
+package com.pingwit.core.les_22.classwork.point_1;
+
+@FunctionalInterface
+public interface SimpleFuncInterface {
+
+    void show();
+
+    default void def1() {
+        //code
+    }
+
+    default void def2() {
+        //code
+    }
+}
+
+class B implements SimpleFuncInterface {
+
+    @Override
+    public void show() {
+        System.out.println("Hi!");
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        SimpleFuncInterface sfi = () -> System.out.println("Hi from lambda expression!!!");
+        sfi.show();
+    }
+}
