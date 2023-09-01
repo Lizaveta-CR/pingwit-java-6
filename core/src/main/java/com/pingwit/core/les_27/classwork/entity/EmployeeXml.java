35ccfb3559c5b8eef3f6ef95d50cbe3667fad6e2
Lizaveta Tsvirko <lizaveta_tsvirko@epam.com>
2023-09-01 10:05:20 +0300

    les_27: age example

--- core/src/main/java/com/pingwit/core/les_27/classwork/entity/EmployeeXml.java
+++ core/src/main/java/com/pingwit/core/les_27/classwork/entity/EmployeeXml.java
@@ -8,15 +8,17 @@ public class EmployeeXml {
     private String firstName;
     private String lastName;
     private String location;
+    private int age;
 
     public EmployeeXml() {
     }
 
-    public EmployeeXml(String id, String firstName, String lastName, String location) {
+    public EmployeeXml(String id, String firstName, String lastName, String location, int age) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.location = location;
+        this.age = age;
     }
 
     public String getId() {
@@ -51,6 +53,14 @@ public class EmployeeXml {
         this.location = location;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -60,13 +70,13 @@ public class EmployeeXml {
             return false;
         }
         EmployeeXml that = (EmployeeXml) o;
-        return Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName)
-            && Objects.equals(location, that.location);
+        return age == that.age && Objects.equals(id, that.id) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName,
+            that.lastName) && Objects.equals(location, that.location);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, location);
+        return Objects.hash(id, firstName, lastName, location, age);
     }
 
     @Override
@@ -76,6 +86,7 @@ public class EmployeeXml {
             ", firstName='" + firstName + '\'' +
             ", lastName='" + lastName + '\'' +
             ", location='" + location + '\'' +
+            ", age=" + age +
             '}';
     }
 }
